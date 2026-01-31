412bf38ad83c93f1f413018bbfa3283bf0d7add2
SURAJ ARS <70328836+SURAJARS@users.noreply.github.com>
2026-01-31 22:32:08 +0530

    Update enquiryRoutes.js

--- backend/src/routes/enquiryRoutes.js
+++ backend/src/routes/enquiryRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
+import { adminAuth } from "../middleware/auth.js";
+import enquiryController from "../controllers/enquiryController.js";
+
 const router = express.Router();
-const { adminAuth } = require('../middleware/auth');
-const enquiryController = require('../controllers/enquiryController');
 
-router.post('/create', enquiryController.createEnquiry);
+router.post("/create", enquiryController.createEnquiry);
+
 // TODO: Re-enable adminAuth after proper admin setup
-router.get('/', enquiryController.getEnquiries);
-router.put('/:id/status', adminAuth, enquiryController.updateEnquiryStatus);
-router.delete('/:id', adminAuth, enquiryController.deleteEnquiry);
+router.get("/", enquiryController.getEnquiries);
 
-module.exports = router;
+router.put("/:id/status", adminAuth, enquiryController.updateEnquiryStatus);
+router.delete("/:id", adminAuth, enquiryController.deleteEnquiry);
 
+export default router;
